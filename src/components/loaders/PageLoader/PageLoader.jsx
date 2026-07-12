75c9d548f439eeb46e882101e97bd92922252d1a
Md. Salman Ezaz <md.salmanezaz@gmail.com>
2026-07-13 04:41:40 +0600

    updated page loader bg

--- src/components/loaders/PageLoader/PageLoader.jsx
+++ src/components/loaders/PageLoader/PageLoader.jsx
@@ -1,7 +1,7 @@
 import "./PageLoader.css";
 export default function PageLoader() {
   return (
-    <div className="fixed top-0 left-0 h-dvh w-full flex items-center justify-center">
+    <div className="fixed top-0 left-0 h-dvh w-full flex items-center justify-center bg-primary/50 z-99">
       <div className="loader"></div>
     </div>
   );
